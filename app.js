890b07004ee007f2b0acead079db61f191d4e53e
Sam Shadwell <sshadwell978@gmail.com>
2024-06-05 15:42:53 -0400

    Completed all exercises

--- app.js
+++ app.js
@@ -5,6 +5,33 @@ console.log(
 
 // Exercise 1
 
+let lastName = "Shadwell";
+var age = 28;
+
 // Exercise 2
 
+const firstName = `Sam`;
+
 // Exercise 3
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = 'M';
+
+// Exercise 5
+
+let title = "Name of the Wind";
+let author = 'Patrick Rothfuss';
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
